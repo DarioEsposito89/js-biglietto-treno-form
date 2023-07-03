71a88708afdde6bbdd899065f19373ada472297b
DarioEsposito89 <darioesposito.work@gmail.com>
2023-07-03 16:39:37 +0200

    completato codice js

---
+++ js/main.js
@@ -0,0 +1,78 @@
+
+const btnGenerate = document.getElementById("generate");
+btnGenerate.addEventListener("click", function () {
+        
+        const passengerName = document.getElementById("name").value;
+        console.log("Generazione nome del passeggero:", passengerName);
+
+        const km = document.getElementById("km").value;
+        console.log("Generazione dei km da percorrere:", km);
+
+        const age = document.getElementById("age").value;
+        console.log("Generazione dell'età:", age);
+
+        const ticketPrice = (km * 0.21);
+        let discount;
+        let discountType;
+        console.log("Prezzo del biglietto pieno:", ticketPrice);
+
+        // calcolo sconto per età
+        if (age == "minorenne") {
+            discount = ticketPrice * 0.2; 
+            discountType = "Biglietto minorenni";
+        } else if (age =="over65") {
+            discount = ticketPrice * 0.4;
+            discountType = "Biglietto over 65";
+        } else {
+            discount = 0;
+            discountType = "Biglietto standard";
+        }
+
+        // calcolo prezzo finale
+        const finalPrice = ticketPrice - discount;
+        console.log("Prezzo dello sconto:", discount);
+        console.log("Prezzo del biglietto finale:", finalPrice);
+        console.log("Tipo di sconto:", discountType);
+
+        // creazione dati biglietto in pagina
+        document.getElementById("passenger").innerHTML = passengerName;
+        document.getElementById("discount").innerHTML = discountType;
+        document.getElementById("ticket-price").innerHTML = finalPrice.toFixed (2) + "€";
+
+        const wagonNumber = Math.floor(Math.random() * 9) + 1;
+        console.log("Creazione numero di carrozza:", wagonNumber);
+        document.getElementById("wagon").innerHTML = wagonNumber;
+
+        const cpCode = Math.floor(Math.random() * 10000) + 90000;
+        console.log("Creazione codice CP:", cpCode);
+        document.getElementById("cp-code").innerHTML = cpCode;
+        
+        // apparizione sezione ticket
+        document.getElementById("ticket-container").className = "bottom show";
+    })
+
+// evento al click del pulsante Annulla
+
+const btnCancel = document.getElementById("cancel");
+btnCancel.addEventListener("click", function () {
+        // sparizione parametri ticket
+        document.getElementById("name").value = "";
+        document.getElementById("km").value = "";
+        document.getElementById("age").value = "";
+
+        document.getElementById("passenger").innerHTML = "";
+        document.getElementById("discount").innerHTML = "";
+        document.getElementById("ticket-price").innerHTML = "";
+        document.getElementById("wagon").innerHTML = "";
+        document.getElementById("cp-code").innerHTML = "";
+
+        // sparizione sezione ticket
+        document.getElementById("ticket-container").className = "bottom hidden";
+    })
+
+
+
+
+
+
+
